e81858e20cadecae89bb15dfc47974b6821adfea
Tanjin Ahamed <tanjin.ahamed123@gmail.com>
2021-04-18 18:12:38 +0600

    client working

---
+++ src/components/Home/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB4BpfXSz43tBdiJx-pwpzFbzUCtbtKD6o",
+  authDomain: "shohoz-sebha.firebaseapp.com",
+  projectId: "shohoz-sebha",
+  storageBucket: "shohoz-sebha.appspot.com",
+  messagingSenderId: "63488225649",
+  appId: "1:63488225649:web:363b0fb1a5516c847706d0"
+};
+  export default firebaseConfig;
