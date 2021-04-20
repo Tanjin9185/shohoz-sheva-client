f4ce7d8390e80efc0b6447c05a510c17fec01a77
Tanjin Ahamed <tanjin.ahamed123@gmail.com>
2021-04-20 11:56:47 +0600

    ok

--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
                         <div class="col-sm-12 col-md-6">
                             <h6>About</h6>
                             <p class="text-justify">
-                                Marriage Media <i>CODE WANTS TO BE SIMPLE </i> is an initiative to help the upcoming programmers with the code. Marriage Media focuses on providing the most efficient code or snippets as the code wants to besimple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.
+                            shohoz-sebha <i>CODE WANTS TO BE SIMPLE </i> is an initiative to help the upcoming programmers with the code. shohoz-sebha focuses on providing the most efficient code or snippets as the code wants to besimple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.
                             </p>
                         </div>
 
@@ -65,7 +65,7 @@ const Footer = () => {
                     <div class="row">
                         <div class="col-md-8 col-sm-6 col-xs-12">
                             <p class="copyright-text">
-                                Copyright &copy; 2021 All Rights Reserved by <a href="/"> Marriage Media</a>.</p>
+                                Copyright &copy; 2021 All Rights Reserved by <a href="/"> shohoz-sebha</a>.</p>
                         </div>
 
                         <div class="col-md-4 col-sm-6 col-xs-12">
